7d6a5ad36b0751b686022acf0e78f808666d7b80
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-06 12:17:55 -0500

    Add example of using more complex JS, string concatenation, inside the JSX curly braces

--- react_intro/react_docs/src/App.js
+++ react_intro/react_docs/src/App.js
@@ -95,4 +95,37 @@ ex:
         />
     );
 
+
+
+    note: you can put more complex expressions inside the JSX curly braces too, like string concatenation:
+
+    ex: 
+
+
+        const user = {
+            name: "Hedy Lamarr",
+            imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+            imageSize: 90
+        }
+
+        export default function Profile() {
+            return (
+                <>
+                    <h1>{user.name}</h1>
+                    <img 
+                        className="avatar"
+                        src={user.imageUrl}
+                        alt={"Photo of " + user.name}
+                        style={{
+                            width: user.imageSize,
+                            height: user.imageSize
+                        }}
+                    />
+                </>
+            );
+        }
+
+
+        in the example, style={{}} is a regular object inside the style={ } JSX curly braces. you can use the style attribute when your styles depend on JS variables
+
 */
