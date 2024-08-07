539dfc67763abe5012c3fd6ac6d7e1878205d5b1
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-07 11:58:01 -0500

    Take notes on what React is

--- react_intro/react_foundations/index.js
+++ react_intro/react_foundations/index.js
@@ -27,6 +27,18 @@ note: a few things to consider when building modern apps:
 for each part of the app, you need to decide whether you will build a solution yourself or use other tools such as packages, libraries, and frameworks
 
 
+** what is React?
+
+note: React is a JS library for building interactive user interfaces
+
+by user interfaces(UI) we mean elements that users see and interact with on screen
+
+by library, we mean React provides helpful functions (API's) to build UI, but leaves it up to the dev where to use those functions in their app.
+
+part of react's success is that it is relatively unopinionated about the other aspects of building apps. this has resulted in a flourishing ecosystem of third party tools and solutions including NExt.js
+
+it also means there is some work put into building things from the ground up including configuring tools and reinventing solutions for common app reqs
+
 
 
 
