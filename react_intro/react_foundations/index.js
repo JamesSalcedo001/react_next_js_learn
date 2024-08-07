388d6d7165debd94cdadbc2c06f4ecf994de0126
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-08-07 11:54:06 -0500

    Take notes on building blocks of web applications

---
+++ react_intro/react_foundations/index.js
@@ -0,0 +1,41 @@
+/*
+
+note: Next.js is a flexible React framework that gives you the building blocks to create fast, full stack web applications
+
+** building blocks of web apps
+
+note: a few things to consider when building modern apps:
+
+- user interface: how users will consume and interact with your app
+
+- routing: how users navigate between diff parts of your app
+
+- data fetching: where data lives and how to get it
+
+- rendering: when and where you render static or dynamic content
+
+- integrations: what third party services you use (for CMS, auth, payments, etc) and how you connect to them
+
+- infrastructure: where you deploy, store, and run your app code (serverless, CDN, edge, etc)
+
+- performance: how to optimize your app for end users
+
+- scalability: how your app adapts to your team, data, and traffic grow
+
+- dev experience: your team's experience building and maintaining your application
+
+for each part of the app, you need to decide whether you will build a solution yourself or use other tools such as packages, libraries, and frameworks
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
